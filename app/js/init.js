1adeabdf2bdbc9b4677b7fa0d6e88e295278083c
annasmykova <anna.smykova@nure.ua>
2017-11-23 23:59:15 +0200

    first page

--- app/js/init.js
+++ app/js/init.js
@@ -77,8 +77,23 @@ $(function () {
     //     $('#order').modal('close');
     //     $('#modal1').modal('open');
     // })
+
     //initiate the plugin and pass the id of the div containing gallery images
-    $("#img_01").elevateZoom({gallery:'gallery_01',  galleryActiveClass: 'active'});
+    // $("#img_01").elevateZoom({gallery:'gallery_01',  galleryActiveClass: 'active'});
+    $("#img_01").elevateZoom({
+        gallery:'gal1',
+        cursor: 'pointer',
+        galleryActiveClass: 'active',
+        imageCrossfade: true,
+        loadingIcon: 'http://www.elevateweb.co.uk/spinner.gif'});
+
+
+    //pass the images to Fancybox
+    // $("#img_01").bind("click", function(e) {
+    //     var ez =   $('#img_01').data('elevateZoom');
+    //     $.fancybox(ez.getGalleryList());
+    //     return false;
+    // });
 
 //pass the images to Fancybox
 
